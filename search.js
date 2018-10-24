656291368746e6ea8bd5af65b469d34608040bee
adinakogan <44442081+adinakogan@users.noreply.github.com>
2018-10-24 19:08:13 +0300

    Add files via upload

---
+++ search.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const fs = require('fs');
+var flag=0;//to chek if there was atlist 1 file with that text
+
+
+
+function searchFilesInDirectory(ext,txt) {
+    if (ext===undefined)//Running application without any parameter prints a simple help message
+       console.log('usage: node search [ext] [text]');
+   else if(txt===undefined)//Running application without any parameter prints a simple help message
+       console.log('usage: node search [ext] [text]');    
+       else{
+    const dir='./';
+    const files = fs.readdirSync(dir);
+    const found = getFilesInDirectory(dir, ext);
+
+    found.forEach(file => {
+        const fileContent = fs.readFileSync(file);
+
+        // We want full words, so we use full word boundary in regex.
+        const regex = new RegExp('\\b' + txt + '\\b');
+        if (regex.test(fileContent)) {
+            console.log(`${file}, `);
+            flag=1;
+        }
+    });
+}
+if (flag===0)
+console.log('no file was founded');
+}
+
+// Using recursion, we find every file with the desired extention, even if its deeply nested in subfolders.
+function getFilesInDirectory(dir, ext) {
+
+    let files = [];
+    fs.readdirSync(dir).forEach(file => {
+        const filePath = path.join(dir, file);
+        const stat = fs.lstatSync(filePath);
+
+        // If we hit a directory, apply our function to that dir. If we hit a file, add it to the array of files.
+        if (stat.isDirectory()) {
+            const nestedFiles = getFilesInDirectory(filePath, ext);
+            files = files.concat(nestedFiles);
+        } else {
+            if (path.extname(file) === ext) {
+                files.push(filePath);
+            }
+        }
+    });
+    return files;
+}
+searchFilesInDirectory('.'+process.argv[2], process.argv[3],);
